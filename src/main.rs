6fff5c24417ece1b395a1a5f8a772f45e7ba788f
Violette <violettether@gmail.com>
2026-08-10 21:24:45 +0530

    Parse torrent metadata and add info command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unused)]
+#![allow(dead_code)]
+
 use std::collections::HashMap;
 use std::env;
 
@@ -10,23 +13,37 @@ use serde::Serialize;
 #[serde(untagged)]
 enum BencodeValue {
     Integer(i64),
-    String(String),
+    String(Vec<u8>),
     List(Vec<BencodeValue>),
-    Dict(HashMap<String, BencodeValue>),
+    Dict(HashMap<Vec<u8>, BencodeValue>),
 }
 
-fn parse_integer(body: &str) -> Option<i64> {
+struct Torrent {
+    announce: String,
+    info: TorrentInfo,
+}
+
+struct TorrentInfo {
+    length: i64,
+    name: String,
+    piece_length: i64,
+    pieces: Vec<u8>,
+}
+
+fn parse_integer(body: &[u8]) -> Option<i64> {
     match body {
-        "0" => Some(0),
+        b"0" => Some(0),
         s => {
             let (is_negative, digits) = s
-                .strip_prefix('-')
+                .strip_prefix(&[b'-'])
                 .and_then(|s| Some((true, s)))
                 .unwrap_or_else(|| (false, s));
-            if digits.is_empty() || digits.starts_with('0') {
+
+            if digits.is_empty() || digits.starts_with(&[b'0']) {
                 None
             } else {
-                digits
+                str::from_utf8(digits)
+                    .ok()?
                     .parse::<i64>()
                     .map(|n| if is_negative { -n } else { n })
                     .ok()
@@ -35,40 +52,51 @@ fn parse_integer(body: &str) -> Option<i64> {
     }
 }
 
-fn parse(input: &str) -> Option<(BencodeValue, &str)> {
-    match input.chars().next() {
+fn parse(input: &[u8]) -> Option<(BencodeValue, &[u8])> {
+    match input.iter().next() {
         Some(c) if c.is_ascii_digit() => {
-            let (len, input) = input.split_once(':')?;
+            let colon_idx = input.iter().position(|&c| c == b':')?;
 
-            let len = len.parse::<usize>().ok()?;
+            let len = std::str::from_utf8(&input[..colon_idx])
+                .ok()?
+                .parse::<usize>()
+                .ok()?;
 
-            let (string, rest) = input.split_at_checked(len)?;
-            Some((BencodeValue::String(string.to_owned()), rest))
+            let (byte_string, rest) = (
+                input.get(colon_idx + 1..colon_idx + 1 + len)?,
+                input.get(colon_idx + 1 + len..)?,
+            );
+
+            Some((BencodeValue::String(byte_string.to_owned()), rest))
         }
-        Some('i') => {
-            let end_idx = input.find('e').unwrap();
+
+        Some(b'i') => {
+            let end_idx = input.iter().position(|&c| c == b'e')?;
             let body = &input[1..end_idx];
-            let rem = if end_idx + 1 < input.len() {
-                &input[end_idx + 1..]
-            } else {
-                ""
-            };
+
+            let rem = input.get(end_idx + 1..)?;
+
             parse_integer(body).map(|x| (BencodeValue::Integer(x), rem))
         }
-        Some('l') => {
+
+        Some(b'l') => {
             let mut v: Vec<BencodeValue> = vec![];
-            let mut input = input.strip_prefix('l')?;
-            if input.starts_with('e') {
-                input = input.strip_prefix('e')?;
+            let mut input = input.strip_prefix(&[b'l'])?;
+
+            if input.starts_with(&[b'e']) {
+                input = input.strip_prefix(&[b'e'])?;
                 return Some((BencodeValue::List(v), input));
             }
+
             let rest;
+
             loop {
                 if let Some((val, rem)) = parse(input) {
                     input = rem;
                     v.push(val);
-                    if input.starts_with('e') {
-                        rest = input.strip_prefix('e')?;
+
+                    if input.starts_with(&[b'e']) {
+                        rest = input.strip_prefix(&[b'e'])?;
                         break;
                     }
                 } else {
@@ -78,25 +106,32 @@ fn parse(input: &str) -> Option<(BencodeValue, &str)> {
 
             Some((BencodeValue::List(v), rest))
         }
-        Some('d') => {
+
+        Some(b'd') => {
             let mut keys = vec![];
             let mut kv = HashMap::new();
-            let mut input = input.strip_prefix('d')?;
-            if input.starts_with('e') {
-                input = input.strip_prefix('e')?;
+
+            let mut input = input.strip_prefix(&[b'd'])?;
+
+            if input.starts_with(&[b'e']) {
+                input = input.strip_prefix(&[b'e'])?;
                 return Some((BencodeValue::Dict(kv), input));
             }
 
             let rest;
+
             loop {
                 if let Some((BencodeValue::String(k), rem)) = parse(input) {
                     input = rem;
+
                     if let Some((v, rem)) = parse(input) {
                         input = rem;
+
                         keys.push(k.clone());
                         kv.insert(k.clone(), v);
-                        if input.starts_with('e') {
-                            rest = input.strip_prefix('e')?;
+
+                        if input.starts_with(&[b'e']) {
+                            rest = input.strip_prefix(&[b'e'])?;
                             break;
                         }
                     } else {
@@ -106,28 +141,102 @@ fn parse(input: &str) -> Option<(BencodeValue, &str)> {
                     return None;
                 }
             }
+
             if !keys.is_sorted() {
                 None
             } else {
                 Some((BencodeValue::Dict(kv), rest))
             }
         }
+
         _ => None,
     }
 }
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if let Some((val, _)) = parse(encoded_value) {
-        match val {
+fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
+    fn convert(value: BencodeValue) -> serde_json::Value {
+        match value {
             BencodeValue::Integer(x) => x.into(),
+
+            // IMPORTANT: preserve Vec<u8> -> JSON array
             BencodeValue::String(s) => s.into(),
-            BencodeValue::List(v) => serde_json::to_value(&v).unwrap(),
-            BencodeValue::Dict(kv) => serde_json::to_value(&kv).unwrap(),
+
+            BencodeValue::List(v) => serde_json::Value::Array(v.into_iter().map(convert).collect()),
+
+            BencodeValue::Dict(kv) => {
+                let mut result = serde_json::Map::new();
+
+                for (k, v) in kv {
+                    let key = String::from_utf8(k)
+                        .unwrap_or_else(|_| panic!("malformed encoded value"))
+                        .to_string();
+
+                    result.insert(key, convert(v));
+                }
+
+                serde_json::Value::Object(result)
+            }
         }
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
     }
+
+    if let Some((val, _)) = parse(encoded_value) {
+        convert(val)
+    } else {
+        panic!("malformed encoded value");
+    }
+}
+
+fn parse_torrent(content: &[u8]) -> Result<Torrent, String> {
+    let (parsed, _) = parse(content).ok_or("Failed to parse the torrent file")?;
+    let BencodeValue::Dict(dict) = &parsed else {
+        return Err("Torrent file should be a bencoded dictionary".into());
+    };
+
+    let BencodeValue::String(announce) = dict
+        .get(&"announce".to_string().into_bytes())
+        .ok_or("couldn't find 'announce' as a key")?
+    else {
+        return Err("'announce' was not a valid bencoded string".into());
+    };
+    let BencodeValue::Dict(info) = &dict
+        .get(&"info".to_string().into_bytes())
+        .ok_or("couldn't find 'info' as a key")?
+    else {
+        return Err("info' was not a valid bencoded dict".into());
+    };
+    let BencodeValue::Integer(length) = &info
+        .get(&"length".to_string().into_bytes())
+        .ok_or("couldn't find 'length' as a key")?
+    else {
+        return Err("length' was not a valid bencoded integer".into());
+    };
+    let BencodeValue::String(name) = &info
+        .get(&"name".to_string().into_bytes())
+        .ok_or("couldn't find 'name' as a key")?
+    else {
+        return Err("name' was not a valid bencoded string".into());
+    };
+    let BencodeValue::Integer(piece_length) = &info
+        .get(&"piece length".to_string().into_bytes())
+        .ok_or("couldn't find 'piece length' as a key")?
+    else {
+        return Err("piece length' was not a valid bencoded integer".into());
+    };
+    let BencodeValue::String(pieces) = &info
+        .get(&"pieces".to_string().into_bytes())
+        .ok_or("couldn't find 'pieces' as a key")?
+    else {
+        return Err("pieces' was not a valid bencoded string".into());
+    };
+    Ok(Torrent {
+        announce: String::from_utf8(announce.clone()).map_err(|e| e.to_string())?,
+        info: TorrentInfo {
+            length: length.clone(),
+            name: String::from_utf8(name.clone()).map_err(|e| e.to_string())?,
+            piece_length: piece_length.clone(),
+            pieces: pieces.clone(),
+        },
+    })
 }
 
 fn main() {
@@ -138,9 +247,18 @@ fn main() {
         eprintln!("Logs:");
 
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = decode_bencoded_value(&encoded_value.clone().into_bytes());
+
         println!("{}", decoded_value.to_string());
+    } else if command == "info" {
+        eprintln!("Logs:");
+        let filename = &args[2];
+        let torrent_file = std::fs::read(filename).expect("File not found");
+        let parsed_torrent = parse_torrent(&torrent_file).unwrap_or_else(|e| panic!("{}", e));
+
+        println!("Tracked URL: {}", parsed_torrent.announce);
+        println!("Length: {}", parsed_torrent.info.length);
     } else {
-        println!("unknown command: {}", args[1])
+        println!("unknown command: {}", args[1]);
     }
 }
